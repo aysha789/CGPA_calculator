e77c5cc37fce16acd6808ae95e2da1a4db9b80a4
aysha789 <ayshasiddika0102636@gmail.com>
2025-03-23 11:50:33 +0600

    calulation update

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,6 +8,15 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String cgpa="";
+  TextEditingController Controller1 =TextEditingController();
+  TextEditingController Controller2 =TextEditingController();
+  TextEditingController Controller3 =TextEditingController();
+  TextEditingController Controller4 =TextEditingController();
+  TextEditingController Controller5 =TextEditingController();
+  TextEditingController Controller6 =TextEditingController();
+  TextEditingController Controller7 =TextEditingController();
+  TextEditingController Controller8 =TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,12 +30,13 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           children: [
             SizedBox(height: 80,),
-            Text("CGPA:",style: TextStyle(color: Colors.deepPurple,fontSize: 20,fontWeight: FontWeight.bold),),
+            Text("CGPA:$cgpa",style: TextStyle(color: Colors.deepPurple,fontSize: 20,fontWeight: FontWeight.bold),),
             SizedBox(height: 80,),
             Row(
               children: [
                 Expanded(
                   child: TextField(
+                    controller: Controller1,
                     decoration: InputDecoration(
                       hintText: "1st Semester",
                       border: OutlineInputBorder(
@@ -40,6 +50,7 @@ class _HomePageState extends State<HomePage> {
                 ),
                 Expanded(
                   child: TextField(
+                    controller: Controller2,
                     decoration: InputDecoration(
                       hintText: "2nd Semester",
                         border: OutlineInputBorder(
@@ -55,6 +66,7 @@ class _HomePageState extends State<HomePage> {
               children: [
                 Expanded(
                   child: TextField(
+                    controller: Controller3,
                     decoration: InputDecoration(
                         hintText: "3rd Semester",
                         border: OutlineInputBorder(
@@ -68,6 +80,7 @@ class _HomePageState extends State<HomePage> {
                 ),
                 Expanded(
                   child: TextField(
+                    controller: Controller4,
                     decoration: InputDecoration(
                         hintText: "4th Semester",
                         border: OutlineInputBorder(
@@ -83,6 +96,7 @@ class _HomePageState extends State<HomePage> {
               children: [
                 Expanded(
                   child: TextField(
+                    controller: Controller5,
                     decoration: InputDecoration(
                         hintText: "5th Semester",
                         border: OutlineInputBorder(
@@ -96,6 +110,7 @@ class _HomePageState extends State<HomePage> {
                 ),
                 Expanded(
                   child: TextField(
+                    controller: Controller6,
                     decoration: InputDecoration(
                         hintText: "6th Semester",
                         border: OutlineInputBorder(
@@ -111,6 +126,7 @@ class _HomePageState extends State<HomePage> {
               children: [
                 Expanded(
                   child: TextField(
+                    controller: Controller7,
                     decoration: InputDecoration(
                         hintText: "7th Semester",
                         border: OutlineInputBorder(
@@ -124,6 +140,7 @@ class _HomePageState extends State<HomePage> {
                 ),
                 Expanded(
                   child: TextField(
+                    controller: Controller8,
                     decoration: InputDecoration(
                         hintText: "8th Semester",
                         border: OutlineInputBorder(
@@ -136,6 +153,18 @@ class _HomePageState extends State<HomePage> {
             ),
             SizedBox(height: 40,),
             InkWell(
+              onTap: (){
+                cgpa= ((double.parse(Controller1.text)*.05) +
+                    (double.parse(Controller2.text)*.05) +
+                    (double.parse(Controller3.text)*.05) +
+                    (double.parse(Controller4.text) *.10)+
+                    (double.parse(Controller5.text)*.15) +
+                    (double.parse(Controller6.text) *.20)+
+                    (double.parse(Controller7.text) *.25)+
+                    (double.parse(Controller8.text) *.15)).toStringAsFixed(2);
+                setState(() {
+                });
+              },
               child: Container(
                 height: 40,
                 width: 110,
